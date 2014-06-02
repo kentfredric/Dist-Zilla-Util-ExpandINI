5d67af641cfa1e6c08fa40acba27588996571d7d
Kent Fredric <kentfredric@gmail.com>
2014-06-02 23:07:25 +1200

    Build results of 77710e6 (on master)

--- lib/Dist/Zilla/Util/ExpandINI.pm
+++ lib/Dist/Zilla/Util/ExpandINI.pm
@@ -3,9 +3,11 @@ use warnings;
 
 package Dist::Zilla::Util::ExpandINI;
 
+our $VERSION = '0.001000';
+
 # ABSTRACT: Read an INI file and expand bundles as you go.
 
-our $VERSION = '0.001000';
+our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
 use Moo;
 
